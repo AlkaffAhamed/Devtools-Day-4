4bf4b7b22de3c7915c4a418e75c66b57de8fb17d
AlkaffAhamed <alkaffahamed@hotmail.com>
2021-11-17 00:24:09 +0800

    Completed

--- README.md
+++ README.md
@@ -1,1 +1,82 @@
 # Devtools Day 4 Assignment
+
+Day 4 assignment for the subject Fundamentals in Developer Tools in PowerX Program
+
+![img](doc/Screenshot1.png)
+
+
+
+## Instructions 
+
+Requires: 
+
+1. Terraform 1.0.5 
+2. AWS CLI with login credentials configured 
+
+### First Time Setup 
+
+If you are loading this for the first time, follow these instructions to set things up. 
+
+1. Make the following changes to `providers.tf`: 
+   Line 10 change to
+
+   ```terraform
+   backend "local" {
+   ```
+
+2. Run the following commands: 
+
+   ```shell
+   terraform init
+   terraform plan
+   terraform apply
+   ```
+
+3. Create an EC2 Key-Pair by running the command below: 
+
+   ```shell
+   aws ec2 create-key-pair --key-name devtools4 --query 'KeyMaterial' --output text > devtools4.pem
+   chmod 400 devtools4.pem
+   ```
+
+   Here, the name of the key is "devtools4". 
+
+4. Make the following changes to `providers.tf`: 
+   Line 10 change back to
+
+5. ```terraform
+   backend "s3" {
+   ```
+
+6. Run the following command to migrate the local state to S3 bucket
+
+   ```shell
+   terraform init -migrate-state
+   ```
+
+7. Login to AWS console and check if the S3 bucket is created with the name "`terraform-state-bucket-alkaff`" and with 1 object "`devtools-day4.tfstate`" 
+
+### Running the Terraform Infrastructure 
+
+1. Run the following commands: 
+
+   ```shell
+   terraform init
+   terraform plan
+   terraform apply
+   ```
+
+   This will spin up an EC2 Instance. on the console, the Public and Private IP address will be printed. 
+
+2. SSH into the Public IP Address: 
+
+   ```shell
+   ssh -A -i "devtools4.pem" ec2-user@[public_ip]
+   ```
+
+3. SSH into the Private IP Address: 
+
+   ```shell
+   ssh -i "devtools4.pem" ec2-user@[private_ip]
+   ```
+
--- main.tf
+++ main.tf
@@ -3,14 +3,14 @@ variable "name" {
   default = "dev-tools-day4"
 }
 
+# variable "bastion_ip" {
+#   type    = string
+#   default = "180.129.98.70" # http://ipv4.icanhazip.com/
+# }
+
 locals {
   ami_id        = "ami-0f511ead81ccde020"
   instance_type = "t2.micro"
-}
-
-variable "local_ip" {
-  type = string
-  default = "180.129.98.78" # http://ipv4.icanhazip.com/
 }
 
 # https://github.com/terraform-aws-modules/terraform-aws-security-group/tree/master/examples/complete
@@ -26,19 +26,32 @@ resource "aws_security_group" "devtools4" {
   vpc_id = data.aws_vpc.default.id
 }
 
-resource "aws_security_group_rule" "ssh_rule" {
+resource "aws_security_group_rule" "ssh_rule_inbound" {
   type              = "ingress"
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
 
-  cidr_blocks       = ["${var.local_ip}/32"]
+  # cidr_blocks       = ["${aws_instance.app_server_4.public_ip}/32"]
+  # cidr_blocks       = ["${var.bastion_ip}/32"]
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.devtools4.id
+}
+
+resource "aws_security_group_rule" "ssh_rule_outbound" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = -1
+
+  cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.devtools4.id
 }
 
 resource "aws_instance" "app_server_4" {
-  ami           = local.ami_id
-  instance_type = local.instance_type
+  ami                         = local.ami_id
+  instance_type               = local.instance_type
+  # associate_public_ip_address = true
 
   tags = {
     Name = var.name
@@ -48,10 +61,14 @@ resource "aws_instance" "app_server_4" {
   key_name = "devtools4"
 }
 
+output "instance_id" {
+  value = aws_instance.app_server_4.id
+}
+
 output "instance_ip" {
   value = aws_instance.app_server_4.public_ip
 }
 
-output "instance_id" {
-  value = aws_instance.app_server_4.id
+output "instance_ip_private" {
+  value = aws_instance.app_server_4.private_ip
 }
